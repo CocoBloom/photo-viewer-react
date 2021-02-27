7d37269ef0fb789481066fdb68e08cc5dc14cf89
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 22:30:22 +0100

    refactor App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from './components/MainPage';
+import PhotoDetailPage from './components/PhotoDetailPage';
 import { PhotoProvider } from "./contexts/PhotoContext";
-
 
 const App = () => {
     let content = (
@@ -10,6 +10,7 @@ const App = () => {
             <PhotoProvider>
                 <div className="App">
                     <Route exact path="/" component={MainPage}/>
+                    <Route exact path="/photo/:id" component={ PhotoDetailPage }/>
                 </div>
             </PhotoProvider>
         </Router>
