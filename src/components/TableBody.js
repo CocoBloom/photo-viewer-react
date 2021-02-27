f510e775411fe551f651a90b4b54f295469831e8
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 12:44:02 +0100

    implement deletePhoto()

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import "./TableDesign.scss";
 import apiService from "../services/ApiService";
 import Axios from "axios";
+import { PhotoContext } from '../contexts/PhotoContext';
+
 
 const TableBody = (props) => {
 
     const photo = props.photo !== undefined ? props.photo : null;
     const photoID = photo.id;
+    const [photos,setPhotos] = useContext(PhotoContext);
+
 
     const editPhoto = () => {
         console.log("edit foto ", photoID);
     }
 
     const deletePhoto = async () => {
-        const response = await apiService.deletePhoto(photoID);
+        await apiService.deletePhoto(photoID);
+        const updatePhotos = photos.filter(
+            (photo) => photo.id !== photoID
+        );
+        setPhotos(updatePhotos);
     }
 
-    if (photo !== null){
-        return (
-            <React.Fragment key={ photo.id }>
-                <td>{ photo.caption }</td>
-                <td>{ photo.photo_credit }</td>
-                <td>{ photo.view_counter }</td>
-                <td className="edit" onClick={editPhoto}>Edit</td>
-                <td className="delete" onClick={deletePhoto}>Delete</td>
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment key={ photo.id }>
+            <td>{ photo.caption }</td>
+            <td>{ photo.photo_credit }</td>
+            <td>{ photo.view_counter }</td>
+            <td className="edit" onClick={editPhoto}>Edit</td>
+            <td className="delete" onClick={deletePhoto}>Delete</td>
+        </React.Fragment>
+    )
 }
 
 export default TableBody;
