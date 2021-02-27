d60f418e18c307d1f2e882805a8cfff7e50f84db
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 22:32:33 +0100

    implement viewPhotoDetails()

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,15 +1,17 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import "./TableDesign.scss";
 import apiService from "../services/ApiService";
-import Axios from "axios";
 import { PhotoContext } from '../contexts/PhotoContext';
-
+import { useHistory } from "react-router-dom";
 
 const TableBody = (props) => {
 
     const photo = props.photo !== undefined ? props.photo : null;
     const photoID = photo.id;
     const [photos,setPhotos] = useContext(PhotoContext);
+    const index = parseInt(props.index) + 1;
+    const history = useHistory();
+
 
     const editPhoto = () => {
         console.log("edit foto ", photoID);
@@ -23,13 +25,19 @@ const TableBody = (props) => {
         setPhotos(updatePhotos);
     }
 
+    const viewPhotoDetails = () => {
+        history.push(`/photo/${photoID}`);
+    }
+
     return (
         <React.Fragment key={ photo.id }>
+            <td>{ index }</td>
             <td>{ photo.caption }</td>
             <td>{ photo.photo_credit }</td>
             <td>{ photo.view_counter }</td>
             <td className="edit" onClick={editPhoto}>Edit</td>
             <td className="delete" onClick={deletePhoto}>Delete</td>
+            <td onClick={viewPhotoDetails}>View</td>
         </React.Fragment>
     )
 }
