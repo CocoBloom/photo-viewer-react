fdf370d968a35f07bd052dceb1e356c137756557
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 03:39:37 +0100

    import MainPage.js into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { MainPage } from './components/MainPage';
+import MainPage from './components/MainPage';
 
 const App = () => {
     let content = (
         <Router>
             <div className="App">
-              <Route exact path="/" component={MainPage}></Route>
+                <Route exact path="/" component={MainPage}></Route>
             </div>
         </Router>
   );
