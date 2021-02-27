a48d6ac7cab48b37a2ce0013cb30a6f5250c1adc
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 22:31:18 +0100

    add PhotoDetailPage.js

---
+++ src/components/PhotoDetailPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import apiService from "../services/ApiService";
+import "./PhotoDetailDesign.scss";
+
+const PhotoDetailPage = () => {
+
+    const { id } = useParams();
+    const [photo, setPhoto] = useState([]);
+
+    useEffect(() => {
+        async function getPhoto(id) {
+            const response = await apiService.getPhotoByID(id);
+            setPhoto(response.data[0]);
+        }
+        getPhoto(id);
+    }, []);
+
+    return (
+        <div className="detail-container">
+            <a href="/">Back to Main</a>
+            <h3>Datail of Photo</h3>
+            <h5>{ photo.photo_credit }</h5>
+            <h5>{ photo.caption }</h5>
+        </div>
+    )
+}
+
+export default PhotoDetailPage;
