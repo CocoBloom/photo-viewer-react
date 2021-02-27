bb4fbf9992f207756ddd9c711368f1ab7af475c3
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 12:43:34 +0100

    change useEffect() with context

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useContext, useState} from 'react';
 import apiService from "../services/ApiService";
 import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./TableDesign.scss";
-import TableBootstrap from 'react-bootstrap/Table'
+import TableBootstrap from 'react-bootstrap/Table';
+import { PhotoContext } from '../contexts/PhotoContext';
+
 
 const Table = () => {
 
-    const [photos,setPhotos] = useState([]);
-
-    useEffect(() => {
-
-        async function getPhotos() {
-            const response = await apiService.getPhotos();
-            setPhotos(response.data);
-        }
-        getPhotos();
-
-    }, []);
+    const [photos,setPhotos] = useContext(PhotoContext);
 
     return (
         <TableBootstrap striped bordered hover variant="dark" className="table">
